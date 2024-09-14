dbd1cb2f31e19e8efb09264ba29c9662d64d55ba
MD55233 <malikdaniyaldawood@gmail.com>
2024-09-15 00:32:27 +0500

    sign-in oage

---
+++ components/CustmonInput.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FormControl, FormLabel, FormField, FormMessage } from './ui/form';
+import { Input } from './ui/input';
+import { Control } from 'react-hook-form';
+import { z } from 'zod';
+import { authFormSchema } from '@/lib/utils';
+
+interface CustmonInputProps {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name:  Control<z.infer<typeof authFormSchema>> ,
+  label: string,
+  placeholder: string
+}
+
+const CustmonInput = ({ control, name, label, placeholder }: CustmonInputProps) => {
+  return (
+    <FormField
+      control={control} // Use control prop instead of form.control
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={name === 'password' ? 'password' : 'text'}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustmonInput;
